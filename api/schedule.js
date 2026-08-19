72c133d17793a3772c8a865baa380f3aa7f54258
Hiura <robinaska444@gmail.com>
2026-08-20 00:50:11 +0700

    Delete api/schedule.js

--- api/schedule.js
+++
@@ -1,9 +0,0 @@
-import { fetchPage, parseCards, BASE } from './_scraper.js';
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const html = await fetchPage(BASE + '/schedule/');
-    const cards = parseCards(html);
-    res.json({ data: [{ day: 'Ongoing', animeList: cards.map(c => ({ anime_name: c.title, cover: c.image, link: c.url })) }] });
-  } catch (e) { res.status(500).json({ error: e.message }); }
-}
